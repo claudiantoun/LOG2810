dcab42c9e188f63e30780229cbc04438e8bbaaae
nicolas-normand <nicolasnormand@hotmail.com>
2018-10-28 23:00:02 -0400

    modifications sur graphElement

--- LOG2810-TP1/src/graphElement.java
+++ LOG2810-TP1/src/graphElement.java
@@ -15,16 +15,20 @@ public class graphElement extends vertex
 	public static void main(String[] args) throws Exception 
 	{ 
 		System.out.println("Veuillez entrer le chemin complet du fichier à lire avec son extension (src/centresLocaux.txt, par exemple): ");
+		
 		Scanner scan = new Scanner(System.in);
 		String userInputFileName = scan.nextLine();
 		File file = new File(userInputFileName); 
+		
 		BufferedReader br = new BufferedReader(new FileReader(file)); 
 		ArrayList<vertex> vertexList = new ArrayList<vertex>();
+		
 		readVertex(br, vertexList);
-		int[][] vertexLinks = links;
+		
 		Double recharge = 0.0;
 		int tempsRecharge = 0;
 		int goToRechargeIndex = -1;
+		
 		displayMenu(vertexList, recharge, tempsRecharge, goToRechargeIndex);
 	}
 } 
